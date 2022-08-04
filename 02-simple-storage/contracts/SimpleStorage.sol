1632f207fa27a52f580fd018713457fa8d534626
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2022-08-04 19:32:38 -0300

    feat: add SimpleStorage contract

---
+++ 02-simple-storage/contracts/SimpleStorage.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+/**
+ * @title SimpleStorage
+ * @dev Store & retrieve a string value in a variable
+ */
+contract SimpleStorage {
+    string private data;
+
+    /**
+     * @dev Store a string value in a storage variable
+     * @param _data value to store
+     */
+    function store(string memory _data) public {
+        data = _data;
+    }
+
+    /**
+     * @dev Return string value from storage
+     * @return value of 'data'
+     */
+    function retrieve() public view returns (string memory) {
+        return data;
+    }
+}
