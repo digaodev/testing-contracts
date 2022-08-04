054f51a4bacba74af8b988dba464cf234efea8ed
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2022-08-04 19:33:47 -0300

    test: add tests for SimpleContract

---
+++ 02-simple-storage/test/SimpleStorage.ts
@@ -0,0 +1,32 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("SimpleStorage", function () {
+  async function deployWithCleanFixture() {
+    const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
+    const simpleStorage = await SimpleStorage.deploy();
+
+    return { simpleStorage };
+  }
+
+  describe("Deployment", function () {
+    it("Should deploy successfully with the right default value", async function () {
+      const { simpleStorage } = await loadFixture(deployWithCleanFixture);
+
+      expect(await simpleStorage.retrieve()).to.equal('');
+    });
+  });
+
+  describe("Store and Retrieve", function () {
+    it("Should store a string value and retrieve it correctly", async function () {
+      const { simpleStorage } = await loadFixture(deployWithCleanFixture);
+      const textToStore = 'John Doe'
+
+      const tx = await simpleStorage.store(textToStore)
+      tx.wait()
+
+      expect(await simpleStorage.retrieve()).to.equal(textToStore);
+    });
+  });  
+});
