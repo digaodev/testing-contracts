8dc60d577d2f0cba2160d6b518c31826d91f00e6
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2022-08-10 15:46:01 -0300

    test: add AdvancedStorage tests

---
+++ 03-advanced-storage/test/AdvancedStorage.ts
@@ -0,0 +1,68 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("AdvancedStorage", function () {
+  async function deployWithCleanFixture() {
+    const AdvancedStorage = await ethers.getContractFactory("AdvancedStorage");
+    const advancedStorage = await AdvancedStorage.deploy();
+
+    return { advancedStorage };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the initial length of the array correctly", async function () {
+      const { advancedStorage } = await loadFixture(deployWithCleanFixture);
+
+      expect(await advancedStorage.length()).to.equal(0);
+    });
+  });
+
+
+  describe("Actions", function () {
+    it("Should add a number to the ids array", async function () {
+      const { advancedStorage } = await loadFixture(deployWithCleanFixture);
+
+      const tx = await advancedStorage.add(10);
+      tx.wait();
+
+      expect(await advancedStorage.get(0)).to.equal(10);
+    });
+
+    it("Should get a number in a specific position in the ids array", async function () {
+      const { advancedStorage } = await loadFixture(deployWithCleanFixture);
+
+      const tx1 = await advancedStorage.add(10);
+      tx1.wait();
+
+      const tx2 = await advancedStorage.add(20);
+      tx2.wait();
+
+      expect(await advancedStorage.get(1)).to.equal(20);
+    });
+
+    it("Should get all numbers in the ids array", async function () {
+      const { advancedStorage } = await loadFixture(deployWithCleanFixture);
+
+      const tx1 = await advancedStorage.add(10);
+      tx1.wait();
+
+      const tx2 = await advancedStorage.add(20);
+      tx2.wait();
+
+      expect(await advancedStorage.getAll()).to.deep.equal([10, 20]);
+    });
+
+    it("Should get the length for the ids array", async function () {
+      const { advancedStorage } = await loadFixture(deployWithCleanFixture);
+
+      const tx1 = await advancedStorage.add(10);
+      tx1.wait();
+
+      const tx2 = await advancedStorage.add(20);
+      tx2.wait();
+
+      expect(await advancedStorage.length()).to.equal(2);
+    });
+  });
+});
