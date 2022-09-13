4b715f1df9c1a3e3c6842079d1fbf9400f1786be
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2022-09-13 20:01:48 -0300

    feat: add project nft-101

---
+++ nft-101/scripts/deploy.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const ERC721 = await ethers.getContractFactory("ERC721");
+  const erc721 = await ERC721.deploy();
+
+  await erc721.deployed();
+
+  console.log(`erc721 deployed to ${erc721.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
