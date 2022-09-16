790e473966d21e2b38985f37b89ddc77fac7da37
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2022-09-16 12:12:30 -0300

    feat: add a new ERC1155-based contract and metadata for a yoshi mint. Add a new depoly script

---
+++ nft-101/contracts/SuperMarioWorldERC1155.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "./ERC1155.sol";
+
+contract SuperMarioWorldERC1155 is ERC1155 {
+    string public name;
+    string public symbol;
+
+    uint256 public tokenCount;
+
+    // tokenId => tokenURI
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+    }
+
+    function uri(uint256 _tokenId) public view returns (string memory) {
+        return _tokenURIs[_tokenId];
+    }
+
+    function mint(uint256 _amount, string memory _tokenURI) public {
+        require(msg.sender != address(0), "Mint to address zero");
+
+        tokenCount += 1;
+
+        _balances[tokenCount][msg.sender] += _amount;
+
+        _tokenURIs[tokenCount] = _tokenURI;
+
+        emit TransferSingle(
+            msg.sender,
+            address(0),
+            msg.sender,
+            tokenCount,
+            _amount
+        );
+    }
+
+    function supportsInterface(bytes4 interfaceID)
+        public
+        pure
+        override
+        returns (bool)
+    {
+        return interfaceID == 0xd9b67a26 || interfaceID == 0x0e89341c;
+    }
+}
